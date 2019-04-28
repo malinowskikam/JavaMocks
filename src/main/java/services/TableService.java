db4edbbe36a6fd7f68fef06d2033a6af92ddad31
malinowskikam <kamiltruskawa@gmail.com>
2019-04-28 16:03:46 +0200

    table service

---
+++ src/main/java/services/TableService.java
@@ -0,0 +1,60 @@
+package services;
+
+import data.Repository;
+import models.Table;
+import errors.EntryNotFoundException;
+import errors.ValidationException;
+
+public class TableService
+{
+    private RestaurantService restaurantService;
+    private Repository database;
+
+    public TableService(Repository db)
+    {
+        database = db;
+        restaurantService = new RestaurantService(db);
+    }
+
+    public Long addTable(Table table) throws ValidationException,EntryNotFoundException
+    {
+        if(!table.isValid())
+            throw new ValidationException("Table",table.getValidationError());
+
+        if(null==restaurantService.get(table.getRestaurantId()))
+            throw new EntryNotFoundException("Restaurant",table.getRestaurantId());
+
+        database.add(table);
+
+        return table.getId();
+    }
+
+    public void deleteTable(Table table) throws EntryNotFoundException
+    {
+        Table t = database.get(table.getId(),Table.class);
+        if(t==null)
+            throw new EntryNotFoundException("Table",table.getId());
+
+        database.delete(table);
+    }
+
+    public void updateTable(Table table) throws ValidationException,EntryNotFoundException
+    {
+        Table t = database.get(table.getId(),Table.class);
+        if(t==null)
+            throw new EntryNotFoundException("Table",table.getId());
+
+        if(null==restaurantService.get(table.getRestaurantId()))
+            throw new EntryNotFoundException("Restaurant",table.getRestaurantId());
+
+        if(!table.isValid())
+            throw new ValidationException("Table",table.getValidationError());
+
+        database.update(table);
+    }
+
+    public Table get(Long id)
+    {
+        return database.get(id,Table.class);
+    }
+}
