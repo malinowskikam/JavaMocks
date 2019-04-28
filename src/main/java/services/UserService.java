705715724b6c3bff10117fd4a22ff3b12be8ab10
malinowskikam <kamiltruskawa@gmail.com>
2019-04-28 15:35:03 +0200

    user service delete method argument changed from id to model

---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,51 @@
+package services;
+
+import data.Repository;
+import models.*;
+import errors.*;
+
+public class UserService
+{
+    private Repository database;
+
+    public UserService(Repository db)
+    {
+        database=db;
+    }
+
+    public Long add(User user) throws ValidationException
+    {
+        if(!user.isValid())
+            throw new ValidationException("User",user.getValidationError());
+
+        database.add(user);
+
+        return user.getId();
+    }
+
+    public void delete(User user) throws EntryNotFoundException
+    {
+        User u = database.get(user.getId(),User.class);
+        if(u==null)
+            throw new EntryNotFoundException("User",user.getId());
+
+        database.delete(u);
+    }
+
+    public void update(User user) throws ValidationException,EntryNotFoundException
+    {
+        User u = database.get(user.getId(),User.class);
+        if(u==null)
+            throw new EntryNotFoundException("User",user.getId());
+
+        if(!user.isValid())
+            throw new ValidationException("User",user.getValidationError());
+
+        database.update(user);
+    }
+
+    public User get(Long id)
+    {
+        return database.get(id,User.class);
+    }
+}
