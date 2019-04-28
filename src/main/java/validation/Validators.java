a8231993fe6b15e277029c3f0700dde1032511a7
malinowskikam <kamiltruskawa@gmail.com>
2019-04-28 14:31:53 +0200

    Validators code cleanup

---
+++ src/main/java/validation/Validators.java
@@ -0,0 +1,27 @@
+package validation;
+
+import org.joda.time.LocalTime;
+
+public class Validators {
+    private static final String email_regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+
+    public static boolean isNotNull(Object o) {
+        return o != null;
+    }
+
+    public static boolean isNotEmpty(String s) {
+        return !"".equals(s);
+    }
+
+    public static boolean isEmail(String s) {
+        return s.matches(email_regex);
+    }
+
+    public static boolean isMultipleOfHalfhour(LocalTime time) {
+        return time.getSecondOfMinute() == 0 && time.getMinuteOfHour() % 30 == 0;
+    }
+
+    public static boolean isPositiveInteger(int i) {
+        return i > 0;
+    }
+}
