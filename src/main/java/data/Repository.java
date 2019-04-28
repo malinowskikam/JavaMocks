e28cac551534186ef2a49dda3838752ca0a07816
malinowskikam <kamiltruskawa@gmail.com>
2019-04-28 15:56:15 +0200

    repository getall method

--- src/main/java/data/Repository.java
+++ src/main/java/data/Repository.java
@@ -1,8 +1,11 @@
 package data;
 
+import java.util.List;
+
 public interface Repository {
     <T> void add(T model);
     <T> void update(T model);
     <T> T get(long id, Class<T> modelClass);
+    <T> List<T> getAll(Class<T> modelClass);
     <T> void delete(T model);
 }
