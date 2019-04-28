277b6ba90bf5ced0166cc83dae6c9809664885d9
malinowskikam <kamiltruskawa@gmail.com>
2019-04-28 15:48:21 +0200

    restaurant service

---
+++ src/main/java/services/RestaurantService.java
@@ -0,0 +1,52 @@
+package services;
+
+import data.Repository;
+import models.Restaurant;
+import errors.EntryNotFoundException;
+import errors.ValidationException;
+
+public class RestaurantService
+{
+    private Repository database;
+
+    public RestaurantService(Repository db)
+    {
+        database = db;
+    }
+
+    public Long add(Restaurant restaurant) throws ValidationException
+    {
+        if(!restaurant.isValid())
+            throw new ValidationException("Restaurant",restaurant.getValidationError());
+
+        database.add(restaurant);
+
+        return restaurant.getId();
+    }
+
+    public void deleteRestaurant(Restaurant restaurant) throws EntryNotFoundException
+    {
+        Restaurant r = database.get(restaurant.getId(),Restaurant.class);
+        if(r==null)
+            throw new EntryNotFoundException("Restaurant",restaurant.getId());
+
+        database.delete(r);
+    }
+
+    public void updateRestaurant(Restaurant restaurant) throws ValidationException,EntryNotFoundException
+    {
+        Restaurant r = database.get(restaurant.getId(),Restaurant.class);
+        if(r==null)
+            throw new EntryNotFoundException("Restaurant",restaurant.getId());
+
+        if(!restaurant.isValid())
+            throw new ValidationException("Restaurant",restaurant.getValidationError());
+
+        database.update(restaurant);
+    }
+
+    public Restaurant getRestaurantById(Long id)
+    {
+        return database.get(id,Restaurant.class);
+    }
+}
