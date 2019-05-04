742a556a284033e79ce894591e51b037415abf17
malinowskikam <kamiltruskawa@gmail.com>
2019-05-04 15:51:37 +0200

    restaurant service get tables and get reservations functions

--- src/main/java/services/RestaurantService.java
+++ src/main/java/services/RestaurantService.java
@@ -1,9 +1,14 @@
 package services;
 
 import data.Repository;
+import models.Reservation;
 import models.Restaurant;
 import errors.EntryNotFoundException;
 import errors.ValidationException;
+import models.Table;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RestaurantService
 {
@@ -48,5 +53,46 @@ public class RestaurantService
     public Restaurant get(Long id)
     {
         return database.get(id,Restaurant.class);
+    }
+
+    public List<Table> getTables(Restaurant restaurant) throws EntryNotFoundException
+    {
+        TableService tableService = new TableService(database);
+        Restaurant r = database.get(restaurant.getId(),Restaurant.class);
+        if(r==null)
+            throw new EntryNotFoundException("Restaurant",restaurant.getId());
+
+        List<Table> tables = database.getAll(Table.class);
+
+        List<Table> restaurantsTables = new ArrayList<>();
+
+        for(Table t : tables)
+        {
+            if(tableService.getRestaurant(t).equals(r))
+                restaurantsTables.add(t);
+        }
+
+        return restaurantsTables;
+    }
+
+    public List<Reservation> getReservations(Restaurant restaurant) throws EntryNotFoundException
+    {
+        ReservationService reservationService = new ReservationService(database);
+        Restaurant r = database.get(restaurant.getId(),Restaurant.class);
+        if(r==null)
+            throw new EntryNotFoundException("Restaurant",restaurant.getId());
+
+        List<Reservation> reservations = database.getAll(Reservation.class);
+        List<Table> tables = getTables(r);
+
+        List<Reservation> restaurantsReservations = new ArrayList<>();
+
+        for(Reservation res : reservations)
+        {
+            if(tables.contains(reservationService.getTable(res)))
+                restaurantsReservations.add(res);
+        }
+
+        return restaurantsReservations;
     }
 }
